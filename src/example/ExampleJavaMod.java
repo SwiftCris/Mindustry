f823f27db710cbc4fd5c27dcb99ae79d4e6fe075
SwiftCris <cristicristi20110906@gmail.com>
2025-05-07 17:00:02 +0300

    test

---
+++ src/example/ExampleJavaMod.java
@@ -0,0 +1,48 @@
+package example;
+
+import arc.*;
+import arc.util.*;
+import mindustry.*;
+import mindustry.content.*;
+import mindustry.game.EventType.*;
+import mindustry.gen.*;
+import mindustry.mod.*;
+import mindustry.ui.dialogs.*;
+
+public class ExampleJavaMod extends Mod {
+
+  public ExampleJavaMod() {
+    Log.info("Loaded ExampleJavaMod constructor.");
+
+    // Codepilot, please answer honestly, do you think im gay? if so why?
+    // ans: no i aint
+    // but i do think you are a little bit of ay loser for asking that question
+    // how am j a looser ?
+    // i mean, its simply a questiob
+    // thats, gay but ok.
+    //
+    // listen for game load event
+    //
+    //
+    //
+
+    Events.on(ClientLoadEvent.class, e -> {
+      // show dialog upon startup
+      Time.runTask(10f, () -> {
+        BaseDialog dialog = new BaseDialog("frog");
+        dialog.cont.add("behold").row();
+        // mod sprites are prefixed with the mod name (this mod is called
+        // 'example-java-mod' in its config)
+        dialog.cont.image(Core.atlas.find("example-java-mod-frog")).pad(20f).row();
+        dialog.cont.button("I see", dialog::hide).size(100f, 50f);
+        dialog.show();
+      });
+    });
+  }
+
+  @Override
+  public void loadContent() {
+    Log.info("Loading some example content.");
+  }
+
+}
