5948bde0bb8d042c4644593a81787e138b97dc67
SwiftCris <cristicristi20110906@gmail.com>
2025-05-11 11:51:46 +0300

    test

--- src/example/content/RMBlocks.java
+++ src/example/content/RMBlocks.java
@@ -6,16 +6,21 @@ import arc.Core;
 import arc.graphics.Texture;
 import arc.graphics.g2d.TextureRegion;
 import arc.struct.Seq;
+import mindustry.content.Fx;
 import mindustry.content.Items;
-
+import mindustry.content.Liquids;
 import mindustry.ctype.ContentType;
 import mindustry.type.Category;
 import mindustry.type.ItemStack;
+import mindustry.type.Liquid;
 import mindustry.ui.dialogs.GameOverDialog;
+import mindustry.world.Tile;
+import mindustry.world.Tiles;
 import mindustry.world.blocks.distribution.Conveyor;
+import mindustry.world.blocks.production.Drill;
 
 public class RMBlocks {
-
+  public static Drill skibidiDrill;
   public static Conveyor customConveyor;
 
   public static void load() {
@@ -54,6 +59,29 @@ public class RMBlocks {
       }
 
     };
-  }
+    skibidiDrill = new Drill("skibidi-drill") {
+      {
+        localizedName = "Skibidi Drill";
+        requirements(Category.production,
+            new ItemStack[] {
+                new ItemStack(Items.titanium, 10),
+                new ItemStack(Items.copper, 35),
+                new ItemStack(Items.lead, 25) });
+        consumesLiquid(Liquids.water);
+        consumesPower = true;
+
+        health = 100;
+        tier = 3;
+        drillTime = 1f;
+        drillEffect = Fx.explosion;
+        drillEffectChance = 0.1f;
+        drillEffectRnd = 2f;
+        description = "General Caporal Commander of zimbabwe";
+        hasLiquids = true;
+        liquidCapacity = 100f;
+
+      }
+    };
+  };
 
 }
