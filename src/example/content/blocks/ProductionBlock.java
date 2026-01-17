663e75c9383c05e98c98ba8413cb1df8fe9a7bbc
SwiftCris <cristicristi20110906@gmail.com>
2026-01-17 15:39:43 +0200

    added stuff

--- src/example/content/blocks/ProductionBlock.java
+++ src/example/content/blocks/ProductionBlock.java
@@ -6,6 +6,7 @@ import example.content.RMBlocks;
 import example.content.RMItems;
 import mindustry.content.Fx;
 import mindustry.content.Items;
+import mindustry.content.Liquids;
 import mindustry.entities.Effect;
 import mindustry.game.Team;
 import mindustry.type.Category;
@@ -16,9 +17,37 @@ import mindustry.world.blocks.production.Drill;
 
 public class ProductionBlock {
   public static Drill platinumExtractor;
+  public static Drill uraniumExtractor;
+
   public static Block leaf;
 
   public static void load() {
+    uraniumExtractor = new Drill("uranium-extractor") {
+      {
+        localizedName = "Uranium Extracter";
+        description = "Extraxts Uranium";
+        health = 100;
+        drillTime = 100f;
+        drillEffect = Fx.smoke;
+        drillEffectChance = 0.5f;
+        drillEffectRnd = 2;
+        buildCostMultiplier = 1.0f;
+        category = Category.production;
+        tier = 4;
+        size = 1;
+
+        // consumesLiquid();
+        requirements(Category.production,
+            ItemStack.with(
+                Items.thorium, 80
+
+            ));
+        consumeLiquid(Liquids.water, 5f);
+        consumePower(5f);
+
+      }
+    };
+
     /*
      * platinumExtractor = new Drill("platinum-extractor") {
      * {
